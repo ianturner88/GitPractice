ed45dd5ff53827183fc85c2ad9ad7b1be8fa5bb3
Ian Turner <ianturner88@gmail.com>
2020-01-30 11:32:11 -0500

    Third commit

--- TestingGrounds/TestingGrounds/TestingGrounds.cpp
+++ TestingGrounds/TestingGrounds/TestingGrounds.cpp
@@ -12,5 +12,19 @@ int main(void)
 	User_Input(variable1, variable2);
 
 	std::cout << "The resulting sum is: " << Add_Numbers(variable1, variable2) << std::endl;
+}
 
+void User_Input(int& variable1, int& variable2)
+{
+	/*Ask for user input*/
+
+	//ask for value of variable one
+	std::cout << "The value of variable one" << std::endl;
+	//set value of variable one
+	std::cin >> variable1;
+
+	//ask for value of variable two
+	std::cout << "The value of variable two" << std::endl;
+	//set value of variable two
+	std::cin >> variable2;
 }
