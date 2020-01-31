e981bd671d1d5e5a2ff0548b165ff18ce8e1d22b
Ian Turner <ianturner88@gmail.com>
2020-01-31 11:36:18 -0500

    First commit

---
+++ Round3/Round3/Round3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void User_Input(int& variable1, int& variable2);
+int Add_Numbers(int variable1, int variable2);
+int Multiply_Numbers(int variable1, int variable2);
+int Subtract_Numbers(int variable1, int variable2);
+
+int main(void)
+{
+	//initialize variables
+	int variable1 = 0, variable2 = 0;
+
+	//ask for user input
+	User_Input(variable1, variable2);
+
+	std::cout << "The resulting sum is: " << Add_Numbers(variable1, variable2) << std::endl;
+	std::cout << "The resulting sum is: " << Subtract_Numbers(variable1, variable2) << std::endl;
+}
